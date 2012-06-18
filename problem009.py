d619181468beb00866a3ba1e65f6ab9bc625325e
Mike Thomson <mike@bluetempest.net>
2012-06-18 23:03:44 +0200

    Ugly but working problem 9

--- problem009.py
+++ problem009.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python
-def 
+def isTriplet(a,b,c):
+	return (a*a + b*b == c*c)
+
+done = False
+for a  in range(1,999) :
+	for b in range(a, 999) :
+		c = 1000 - a - b
+		if isTriplet(a,b,c) :
+			done = True
+			break
+	if done :
+		break
+print a*b*c
